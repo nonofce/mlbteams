645fb9c824ad81b731119971fdb840780bc954be
Nono F. Carballo Escalona <nonofce@gmail.com>
2019-12-29 15:16:32 -0500

    Create about screen (close #10)

---
+++ app/src/main/java/com/nonofce/android/mlbteams/ui/AboutFragment.kt
@@ -0,0 +1,29 @@
+package com.nonofce.android.mlbteams.ui
+
+
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.nonofce.android.mlbteams.R
+import kotlinx.android.synthetic.main.fragment_about.*
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        poweredByUrl.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+}
