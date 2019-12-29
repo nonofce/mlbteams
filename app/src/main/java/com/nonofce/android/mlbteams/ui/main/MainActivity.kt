e3dc8b021f3493b97b0b32a97946c0602181da4c
Nono F. Carballo Escalona <nonofce@gmail.com>
2019-12-29 17:52:39 -0500

    Adds navigation drawer (close #9)

--- app/src/main/java/com/nonofce/android/mlbteams/ui/main/MainActivity.kt
+++ app/src/main/java/com/nonofce/android/mlbteams/ui/main/MainActivity.kt
@@ -2,13 +2,39 @@ package com.nonofce.android.mlbteams.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.nonofce.android.mlbteams.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.teamFragment, R.id.settingsFragment, R.id.aboutFragment),
+            drawerLayout
+        )
+
+        navController = findNavController(R.id.myNavHostFragment)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        nav_view.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
